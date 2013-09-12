75458bda0e5697116c6f7905c6614920cb563897
Michael Kelly <michaelkelly322@gmail.com>
2013-09-12 16:23:21 -0400

    Began work on BMS_ConfigManager.  Created class diagrams for all related objects/classes and generated stub code.

--- BMS.Core.ModelsLib/Generated Code/BMS/Core/BMS_ConfigManager.cs
+++ BMS.Core.ModelsLib/Generated Code/BMS/Core/BMS_ConfigManager.cs
@@ -19,14 +19,45 @@ namespace BMS.Core
 		/// <summary>
 		/// The collection of loaded configs accessible at O(1) relative performance
 		/// </summary>
-		protected static Dictionary<String, BMS_Config> m_configs{}
+		protected virtual Dictionary<String, BMS_Config> m_configs
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Singleton instance of BMS_ConfigManager
+		/// </summary>
+		private static BMS_ConfigManager m_instance
+		{
+			get;
+			set;
+		}
+
+		protected virtual BMS_Logger m_logger
+		{
+			get;
+			set;
+		}
+
+		protected virtual Dictionary<String,XMLSchema> m_schemas
+		{
+			get;
+			set;
+		}
+
+		public virtual IEnumerable<BMS_Config> BMS_Config
+		{
+			get;
+			set;
+		}
 
 		/// <summary>
 		/// Loads a configuration from a specific config file.
 		/// </summary>
 		/// <param name="in_configName">The internal name of the new configuration</param>
 		/// <param name="in_fileName">The file name (including full path) of the configuration file to load</param>
-		public static BMS_Config loadConfigFile(string in_configName, string in_fileName)
+		public virtual BMS_Config loadConfigFile(string in_configName, string in_fileName)
 		{
 			throw new System.NotImplementedException();
 		}
@@ -36,7 +67,7 @@ namespace BMS.Core
 		/// </summary>
 		/// <param name="in_fileName">The filename (including full path) within which to store the configuration</param>
 		/// <param name="in_settings">The BMS_Config object to be stored.</param>
-		public static void saveConfigFile(string in_fileName, BMS_Config in_settings)
+		public virtual void saveConfigFile(string in_fileName, BMS_Config in_settings)
 		{
 			throw new System.NotImplementedException();
 		}
@@ -45,7 +76,32 @@ namespace BMS.Core
 		/// Accesses the configuration of the provided name.
 		/// </summary>
 		/// <param name="in_configName">The name of the configuration to query</param>
-		public static BMS_Config getConfig(string in_configName)
+		public virtual BMS_Config getConfig(string in_configName)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <summary>
+		/// Hidden singleton constructor
+		/// </summary>
+		private BMS_ConfigManager()
+		{
+		}
+
+		/// <summary>
+		/// Provides access to the singleton BMS_ConfigManager instance
+		/// </summary>
+		public static BMS_ConfigManager GetInstance()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <summary>
+		/// Sets the schema with which to validate subsequent config loads
+		/// </summary>
+		/// <param name="in_schemaName">The name of this schema for filling in the Manager</param>
+		/// <param name="in_fileName">The file name (including full path) to the schema definition.</param>
+		public virtual void setConfigSchemaFile(string in_schemaName, string in_fileName)
 		{
 			throw new System.NotImplementedException();
 		}
